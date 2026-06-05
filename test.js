a396bc8b4cda98ee72d91575f0343eeccf2dc354
TW96070 <t.wata96070@gmail.com>
2026-06-05 15:21:41 +0900

    削除

--- test.js
+++
@@ -1,7 +0,0 @@
-// test.js
-const someFunction = require('./index'); // 例
-
-test('ステータスが正常であること', () => {
-    const status = someFunction();
-    expect(status).toBe(true); // 判定はJestに任せる（process.exitは書かない）
-});
