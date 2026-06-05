5156bac4483d1c81af1802550504fdf3703c79b1
TW96070 <t.wata96070@gmail.com>
2026-06-05 15:04:28 +0900

    修正

--- warikan.test.js
+++ warikan.test.js
@@ -4,7 +4,7 @@ describe('割り勘計算のテスト', () => {
 
     test('3000円を3人で分けると、全員1000円になること', () => {
         const result = splitBill(3000, 3);
-        expect(result).toEqual([999, 999, 999]);
+        expect(result).toEqual([1000, 1000, 1000]);
     });
 
     test('3001円を3人で分けると、1人目が1001円、他が1000円になること', () => {
